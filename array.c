4c1e5d2bd4e4d765186a8007a223dba93a8729e2
karansoni7773 <156766722+karansoni773@users.noreply.github.com>
2024-08-03 17:55:09 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define size 100
+void main()
+{
+    int n,i,arr[size];
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter %d elements in the array:\n",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Traversal Start:\n");
+     for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+
+}
